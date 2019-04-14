303f253eba2c5effc7fabe4c5482c4cc1dac47eb
Levent Kıran <leventkiran@hotmail.com.tr>
2019-04-14 15:42:00 +0300

    Database Fix & Django Admin setup
    + Admin models are registered
    + Field classes are edited
    + Ondelete actions are replaces as DO_NOTHING
    + Taks's ExecutedBy field fixed as ForeignKey
    + Task has ForeignKey relation with Workflow
    - Task ManyToMany relation removed from Workflow
    + Following field renamigns are made.
        Treasure -> Saved
        Users -> AssignedUsers
        Roles -> AssignedRoles

--- api/cc/admin.py
+++ api/cc/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(BaseModel)
+admin.site.register(Role)
+admin.site.register(Category)
+admin.site.register(DataEnumeration)
+admin.site.register(DataField)
+admin.site.register(DataType)
+admin.site.register(Task)
+admin.site.register(ExecutionData)
+admin.site.register(Execution)
+admin.site.register(Workflow)
+admin.site.register(Community)
+admin.site.register(Member)
--- api/cc/models.py
+++ api/cc/models.py
@@ -6,24 +6,23 @@ from django.db import models
 from django.utils import timezone
 from jsonfield import JSONField
 
+
 class ClassEnum(Enum):
-	Error = 0
+	NotSpecified = 0
 	Integer = 1
 	String = 2
 	Boolean = 3
-	Color = 4
-	DayOfWeek = 5
-	Date = 6
-	Time = 7
-	Empty = 8
-	DateTime = 9
-	Relation = 10
+	DateTime = 4
+	GeoLocation = 5
+	Relation = 6
+	User = 7
+
 
 class BaseModel(models.Model):
-	CreatedOn = models.DateTimeField()
-	ModifiedOn = models.DateTimeField()
-	Deleted = models.BooleanField(default = False)
-
+	CreatedOn = models.DateTimeField(editable=False)
+	ModifiedOn = models.DateTimeField(editable=False)
+	Deleted = models.BooleanField(default=False)
+	
 	def save(self, *args, **kwargs):
 		if not self.id:
 			self.CreatedOn = timezone.now()
@@ -32,121 +31,130 @@ class BaseModel(models.Model):
 		self.ModifiedOn = timezone.now()
 		return super(BaseModel, self).save(*args, **kwargs)
 
-class Role(BaseModel):
-	Name = models.CharField(max_length = 50)
-	SystemDefined = models.BooleanField(default = True)
 
+class Role(BaseModel):
+	Name = models.CharField(max_length=50)
+	SystemDefined = models.BooleanField(default=True)
+	
 	def __str__(self):
 		return u'Role {0} ({1})'.format(self.Name, self.id)
-
+	
 	def __unicode__(self):
 		return u'Role {0} ({1})'.format(self.Name, self.id)
+
 
 class Category(BaseModel):
-	Name = models.CharField(max_length = 100)
-
+	Name = models.CharField(max_length=100)
+	
 	def __str__(self):
 		return u'Category {0} ({1})'.format(self.Name, self.id)
-
+	
 	def __unicode__(self):
 		return u'Category {0} ({1})'.format(self.Name, self.id)
+
 
 class DataEnumeration(BaseModel):
-	Name = models.CharField(max_length = 50)
-	Value = JSONField(default = "{}")
-
+	Name = models.CharField(max_length=50)
+	Value = JSONField(default="{}")
+	
 	def __str__(self):
 		return u'DataEnumeration {0} ({1})'.format(self.Name, self.id)
-
+	
 	def __unicode__(self):
 		return u'DataEnumeration {0} ({1})'.format(self.Name, self.id)
+
 
 class DataField(BaseModel):
-	Name = models.CharField(max_length = 50)
-	Type = models.IntegerField(choices = [(choice.value, choice.name.replace("_", " ")) for choice in ClassEnum])
-	Enumerations = models.ManyToManyField(DataEnumeration, blank = True)
-	Treasure = models.BooleanField(default = False)
-
+	Name = models.CharField(max_length=50)
+	Type = models.IntegerField(choices=[(choice.value, choice.name.replace("_", " ")) for choice in ClassEnum])
+	Enumerations = models.ManyToManyField(DataEnumeration, blank=True)
+	Saved = models.BooleanField(default=False)
+	
 	def __str__(self):
 		typeName = ClassEnum(int(self.Type)).name.replace('_', ' ')
 		return u'{0} DataField {1} ({2})'.format(typeName, self.Name, self.id)
-
+	
 	def __unicode__(self):
 		typeName = ClassEnum(int(self.Type)).name.replace('_', ' ')
 		return u'{0} DataField {1} ({2})'.format(typeName, self.Name, self.id)
+
 
 class DataType(BaseModel):
-	Name = models.CharField(max_length = 50)
-	Fields = models.ManyToManyField(DataField, blank = True)
-
+	Name = models.CharField(max_length=50)
+	Fields = models.ManyToManyField(DataField, blank=True)
+	
 	def __str__(self):
 		return u'DataType {0} ({1})'.format(self.Name, self.id)
-
+	
 	def __unicode__(self):
 		return u'DataType {0} ({1})'.format(self.Name, self.id)
 
-class Task(BaseModel):
-	Name = models.CharField(max_length = 50)
-	Description = models.CharField(max_length = 5000)
-	Available = models.BooleanField(default = False)
-	AvailableTill = models.DateField(blank = True)
-	AvailableTimes = models.IntegerField(default = 0)
-	Users = models.ManyToManyField(User, blank = True)
-	Roles = models.ManyToManyField(Role, blank = True)
-	Predecessors = models.ManyToManyField("self", blank = True, symmetrical = False)
-	InputFields = models.ManyToManyField(DataField, related_name = "InputFields", blank = True)
-	OutputFields = models.ManyToManyField(DataField, related_name = "OutputFields", blank = True)
-
-	def __str__(self):
-		return u'Task {0} ({1})'.format(self.Name, self.id)
-
-	def __unicode__(self):
-		return u'Task {0} ({1})'.format(self.Name, self.id)
-
-class ExecutionData(BaseModel):
-	Field = models.ForeignKey(DataField, blank = True, on_delete = models.CASCADE)
-	DataGroup = models.ForeignKey("self", blank = True, on_delete = models.CASCADE)
-	Value = JSONField(default = "{}")
-
-class Execution(BaseModel):
-	Task = models.ForeignKey(Task, blank = True, on_delete = models.CASCADE)
-	Data = models.ManyToManyField(ExecutionData, blank = True)
-	ExecutedBy = models.ManyToManyField(User, blank = True)
 
 class Workflow(BaseModel):
-	Name = models.CharField(max_length = 50)
-	Description = models.CharField(max_length = 5000)
-	Users = models.ManyToManyField(User, blank = True)
-	Roles = models.ManyToManyField(Role, blank = True)
-	Tasks = models.ManyToManyField(Task, blank = True)
-
+	Name = models.CharField(max_length=50)
+	Description = models.CharField(max_length=5000)
+	
 	def __str__(self):
 		return u'Workflow {0} ({1})'.format(self.Name, self.id)
-
+	
 	def __unicode__(self):
 		return u'Workflow {0} ({1})'.format(self.Name, self.id)
+
+
+class Task(BaseModel):
+	Name = models.CharField(max_length=50)
+	Description = models.CharField(max_length=5000)
+	Available = models.BooleanField(default=False)
+	AvailableTill = models.DateField(blank=True)
+	AvailableTimes = models.IntegerField(default=0)
+	Workflow = models.ForeignKey(Workflow, blank=True, on_delete=models.DO_NOTHING)
+	AssignedUsers = models.ManyToManyField(User, blank=True)
+	AssignedRoles = models.ManyToManyField(Role, blank=True)
+	Predecessors = models.ManyToManyField("self", blank=True, symmetrical=False)
+	InputFields = models.ManyToManyField(DataField, related_name="InputFields", blank=True)
+	OutputFields = models.ManyToManyField(DataField, related_name="OutputFields", blank=True)
+	
+	def __str__(self):
+		return u'Task {0} ({1})'.format(self.Name, self.id)
+	
+	def __unicode__(self):
+		return u'Task {0} ({1})'.format(self.Name, self.id)
+
+
+class ExecutionData(BaseModel):
+	Field = models.ForeignKey(DataField, blank=True, on_delete=models.DO_NOTHING)
+	DataGroup = models.ForeignKey("self", blank=True, on_delete=models.DO_NOTHING)
+	Value = JSONField(default="{}")
+
+
+class Execution(BaseModel):
+	Task = models.ForeignKey(Task, blank=True, on_delete=models.DO_NOTHING)
+	Data = models.ManyToManyField(ExecutionData, blank=True)
+	ExecutedBy = models.ForeignKey(User, blank=True, on_delete=models.DO_NOTHING)
+
 
 class Community(BaseModel):
-	Name = models.CharField(max_length = 50)
-	Purpose = models.CharField(max_length = 250)
-	Description = models.CharField(max_length = 5000)
-	Roles = models.ManyToManyField(Role, blank = True)
-	Categories = models.ManyToManyField(Category, blank = True)
-
+	Name = models.CharField(max_length=50)
+	Purpose = models.CharField(max_length=250)
+	Description = models.CharField(max_length=5000)
+	Roles = models.ManyToManyField(Role, blank=True)
+	Categories = models.ManyToManyField(Category, blank=True)
+	
 	def __str__(self):
 		return u'Community {0} ({1})'.format(self.Name, self.id)
-
+	
 	def __unicode__(self):
 		return u'Community {0} ({1})'.format(self.Name, self.id)
 
-class Member(BaseModel):
-	Community = models.ForeignKey(Community, on_delete = models.CASCADE)
-	User = models.ForeignKey(User, on_delete = models.CASCADE)
-	Roles = models.ManyToManyField(Role, blank = True)
-	Banned = models.BooleanField(default = False)
 
+class Member(BaseModel):
+	Community = models.ForeignKey(Community, on_delete=models.DO_NOTHING)
+	User = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+	Roles = models.ManyToManyField(Role, blank=True)
+	Banned = models.BooleanField(default=False)
+	
 	def __str__(self):
 		return u'{0} is member of {1}'.format(self.User.first_name, self.Community.Name)
-
+	
 	def __unicode__(self):
 		return u'{0} is member of {1}'.format(self.User.first_name, self.Community.Name)
