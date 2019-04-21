1df0e616e9bcfc860c9f17bacdfd07e3a6200ae4
Levent Kıran <leventkiran@hotmail.com.tr>
2019-04-21 18:40:37 +0300

    Fix

--- api/cc/Services/CategoryService.py
+++ api/cc/Services/CategoryService.py
@@ -4,14 +4,20 @@ import logging
 class CategoryService(object):
 	__instance = None
 	__logger = logging.getLogger('CategoryService')
-
+	
 	@staticmethod
 	def Instance():
 		if CategoryService.__instance is None:
 			CategoryService()
 		return CategoryService.__instance
-
+	
 	def __init__(self):
 		if CategoryService.__instance is not None:
 			raise Exception("CategoryService is a singleton, use 'CategoryService.Instance()'")
 		CategoryService.__instance = self
+	
+	def Search(self, word):
+		raise NotImplementedError
+
+	def Create(self, category):
+		raise NotImplementedError
--- api/cc/Services/CommunityService.py
+++ api/cc/Services/CommunityService.py
@@ -4,14 +4,29 @@ import logging
 class CommunityService(object):
 	__instance = None
 	__logger = logging.getLogger('CommunityService')
-
+	
 	@staticmethod
 	def Instance():
 		if CommunityService.__instance is None:
 			CommunityService()
 		return CommunityService.__instance
-
+	
 	def __init__(self):
 		if CommunityService.__instance is not None:
 			raise Exception("CommunityService is a singleton, use 'CommunityService.Instance()'")
 		CommunityService.__instance = self
+	
+	def Get(self, id):
+		raise NotImplementedError
+	
+	def GetRecent(self, count):
+		raise NotImplementedError
+	
+	def GetPopular(self, count):
+		raise NotImplementedError
+
+	def Create(self, community):
+		raise NotImplementedError
+
+	def Activate(self, community):
+		raise NotImplementedError
