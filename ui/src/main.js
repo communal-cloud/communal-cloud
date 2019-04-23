380a7defd92c45da049ed004594d8967af1839f3
yener <yenerunver@hotmail.com>
2019-04-24 00:23:30 +0300

    Metronic theme integration started

--- ui/src/main.js
+++ ui/src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+//import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import 'sweetalert';
 
 Vue.config.productionTip = false
@@ -12,7 +14,7 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 
 new Vue({
-  store,
-  router,
-  render: h => h(App)
+    store,
+    router,
+    render: h => h(App)
 }).$mount('#app')
