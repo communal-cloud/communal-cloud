ea84dc2067f02cb12607147825e404fcdde6df3f
cagri <cagri.gunal@gmail.com>
2019-04-23 17:34:33 +0300

    error fix update

---
+++ communal-cloud-ui/src/components/CreateTask.vue
@@ -0,0 +1,97 @@
+<template>
+<div>   
+    <b-input-group 
+    prepend="Name" 
+    class="mt-3"
+    id="task_name"
+    description="description of the community"
+    label="Enter task name"
+    label-for="task_name"
+   
+    >
+    <b-form-input v-model="task_name" type="text"/>
+   
+  </b-input-group>
+
+  <b-form-select v-model="task_roles" :options="community_roles" multiple></b-form-select>
+
+<ul id="categories">
+      <li v-for="role in task_roles" :key="role">
+        <b-button v-on:click="RemoveTaskRole(role)">{{ role }}</b-button>
+      </li>
+    </ul>
+
+<div class="row"><div class="col">
+      <b-form-select v-model="task_datas" multiple >
+         <option v-for="field in community_data" :key="field" :value="field.name">{{ dataTypeName }} - {{ field.name }} ({{ field.type }})</option>
+      </b-form-select>
+    </div>
+    <div class="col">
+  <ul id="categories">
+      <li v-for="data in task_datas" :key="data">
+        <b-button >{{ data }}</b-button>
+            <b-form-group label="Field Options">
+      <b-form-checkbox-group
+        v-model="selected"
+        :options="field_options"
+        name="flavour-2a"
+        stacked
+      ></b-form-checkbox-group>
+    </b-form-group>
+      </li>
+    </ul>
+    </div>
+    </div>
+
+     <b-button  v-on:click="toggleModal()" variant="outline-primary">Create</b-button>
+</div>
+
+  </template>
+  <script>
+  import {mapState, mapMutations} from 'vuex';
+
+  export default {
+        computed: {
+   ...mapState([
+     'community_roles',
+     'community_data',
+     'dataTypeName'
+   ])
+    },
+    
+    data() {
+        return {
+            task_name:'',
+            task_role:'',
+            task_roles:[],
+            task_data:'',
+            task_datas:[]
+            
+        }
+
+    },
+    methods : {
+
+     AddTaskRole(){
+       this.task_roles.push(this.task_role);
+     },
+     AddWorkflowRole(){
+       this.workflow_roles.push(this.workflow_role);
+     },
+     AddWorkflowAssign(){
+       this.workflow_assigns.push(this.workflow_assign);
+     },
+     AddTaskData(){
+       this.task_datas.push(this.task_data);
+     },
+     
+     
+     
+     
+     RemoveTaskRole(){
+       
+     },
+    }
+
+  }
+  </script>
---
+++ communal-cloud-ui/src/components/SignIn.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    
+    <b-input-group 
+    prepend="e-mail" 
+    class="mt-3"
+    id="fieldset1"
+    description="Let us know your email."
+    label="Enter your email"
+    label-for="input1"
+    :invalid-feedback="invalidFeedback"
+    :valid-feedback="validFeedback"
+    :state="state">
+    <b-form-input />
+   
+  </b-input-group>
+   <b-input-group 
+    prepend="Password" 
+    class="mt-3"
+    id="fieldset1"
+    label="Enter your password"
+    label-for="input1"
+    :invalid-feedback="invalidFeedback"
+    :valid-feedback="validFeedback"
+    :state="state">
+    <b-form-input type="password" />
+   
+  </b-input-group>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      state() {
+        return this.name.length >= 4 ? true : false
+      },
+      invalidFeedback() {
+        if (this.name.length > 4) {
+          return ''
+        } else if (this.name.length > 0) {
+          return 'Enter at least 4 characters'
+        } else {
+          return 'Please enter something'
+        }
+      },
+      validFeedback() {
+        return this.state === true ? 'Thank you' : ''
+      }
+    },
+    data() {
+      return {
+        name: ''
+      }
+    }
+  }
+</script>
