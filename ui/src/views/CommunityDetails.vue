4e551958ee6f7a9f1afaf6c92d55395d6e3c3e91
Recreation34 <fatihparlayan34@gmail.com>
2019-05-25 22:30:49 +0300

    community detail page added

--- ui/src/views/CommunityDetails.vue
+++ ui/src/views/CommunityDetails.vue
@@ -13,14 +13,6 @@
             parturient montes, nascetur ridiculus mus. Donec consequat orci consectetur porta commodo. Duis faucibus
             purus vel consequat laoreet.</p>
 
-        <div>
-            <b-nav vertical class="w-25">
-                <b-nav-item active>Active</b-nav-item>
-                <b-nav-item>Link</b-nav-item>
-                <b-nav-item>Another Link</b-nav-item>
-                <b-nav-item disabled>Disabled</b-nav-item>
-            </b-nav>
-        </div>
 
     </div>
 </template>
